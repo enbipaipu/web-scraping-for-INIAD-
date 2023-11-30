f113c389d41f1ca309770736bf5612501ba65a5e
enbipaipu <s1f102300122@iniad.org>
2023-11-30 13:54:45 +0900

    test1

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -1,5 +1,6 @@
-print("-------")
-print("111111")
-print("222222")
-print("333333")
-print("-------")
+def test_function():
+    print("-------")
+    print("111111")
+    print("222222")
+    print("333333")
+    print("-------")
