466c4fdc4ae39529027ab32fb821764822f97158
enbipaipu <s1f102300122@iniad.org>
2023-12-04 01:39:43 +0900

    test-12

--- lambda/index.js
+++ lambda/index.js
@@ -1,9 +1,9 @@
 function logValues(x) {
   console.log("testを開始します。");
   let value = ["a", "b", "c"];
-  if (x.length > 0) {
-    value = x;
-  }
+  // if (x.length > 0) {
+  //   value = x;
+  // }
   for (const s in value) {
     console.log(s);
   }
