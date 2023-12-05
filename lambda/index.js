9141f868d0eda95ce5f22c0e56aa120753b07f09
enbipaipu <s1f102300122@iniad.org>
2023-12-05 09:38:18 +0900

    for() -> forEach

--- lambda/index.js
+++ lambda/index.js
@@ -1,12 +1,13 @@
 function logValues(x) {
   console.log("testを開始します。");
+
   let value = ["a", "b", "c"];
-  // if (x.length > 0) {
-  //   value = x;
-  // }
-  for (const s in value) {
-    console.log(s);
+  if (x.length > 0) {
+    value = x;
   }
+  value.forEach((val) => {
+    console.log(val);
+  });
 }
 
 // logValues関数をエクスポートする
