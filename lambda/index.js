e8219f45430768bfa6cf39359a0ca47b742ef633
enbipaipu <s1f102300122@iniad.org>
2023-12-05 10:04:07 +0900

    test14

--- lambda/index.js
+++ lambda/index.js
@@ -3,12 +3,14 @@ function logValues(x) {
 
   let value = ["a", "b", "c"];
   if (x.length > 0) {
+    value.splice(0);
     value = x;
   }
   value.forEach((val) => {
     console.log(val);
   });
 }
+logValues([100, 200, 300]);
 
 // logValues関数をエクスポートする
 module.exports.logValues = logValues;
