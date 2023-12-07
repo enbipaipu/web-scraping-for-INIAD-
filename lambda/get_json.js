534f925157804a0d33c6ab4951fc34e8995d4fd7
enbipaipu <s1f102300122@iniad.org>
2023-12-07 20:26:24 +0900

    add get_json

---
+++ lambda/get_json.js
@@ -0,0 +1,24 @@
+const fetch = require("node-fetch");
+const fs = require("fs/promises");
+
+async function get_json() {
+  console.log("tttttttttttttt");
+  try {
+    const fetch = await import("node-fetch");
+    const response = await fetch.default(
+      "https://github.com/jun-eg/test-zip/blob/main/data/slide.json"
+    );
+    const data = await response.json();
+    fs.writeFile(
+      "test.json",
+      JSON.stringify(data.payload.blob.rawLines, null, 2)
+    );
+
+    return "JSON データを test.json に書き込みました。";
+  } catch (error) {
+    console.error("データを取得できませんでした:", error);
+  }
+}
+get_json();
+
+module.exports.get_json = get_json;
--- lambda/index.js
+++ lambda/index.js
@@ -1,3 +1,5 @@
+const { get_json } = require("./get_json");
+
 function logValues(x) {
   console.log("testを開始します。");
 
@@ -10,6 +12,13 @@ function logValues(x) {
     console.log(val);
   });
 }
-
+async function main() {
+  console.log("mainを実行します");
+  const json = await get_json(); //jsonから保存済みのURLの情報を取得する関数。
+  console.log("return_json : ", json);
+  // let new_info = await get_update_info(); //infoにないURLをmoocsから取得する関数。
+  // await deploy_json(); //new_infoをjsonにデプロイする関数。
+}
+main();
 // logValues関数をエクスポートする
 module.exports.logValues = logValues;
