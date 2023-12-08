effc63e4e7e6f88874f1e3f09d8eb42495451022
enbipaipu <s1f102300122@iniad.org>
2023-12-08 13:40:03 +0900

    test

--- lambda/index.js
+++ lambda/index.js
@@ -8,15 +8,15 @@ function logValues(x) {
     value.splice(0);
     value = x;
   }
-  value.forEach((val) => {
-    console.log(val);
+  value.forEach((test) => {
+    console.log(test);
   });
 }
 async function main() {
   console.log("mainを実行します");
   const json = await get_json(); //jsonから保存済みのURLの情報を取得する関数。
   console.log("return_json : ", json);
-  // let new_info = await get_update_info(); //infoにないURLをmoocsから取得する関数。
+  // let new_info = await get_update_info(); //infoにないURLをmoocsから取得する関数
   // await deploy_json(); //new_infoをjsonにデプロイする関数。
 }
 main();
