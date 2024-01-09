947f19de0b9e758a76a2afadb401fa5e16578c34
enbipaipu <s1f102300122@iniad.org>
2024-01-09 13:20:45 +0900

    deploy方法を模索中2

--- lambda/deploy_json.js
+++ lambda/deploy_json.js
@@ -4,75 +4,51 @@ const path = require("path");
 require("dotenv").config();
 const { Octokit } = require("@octokit/rest");
 
-function change(currentTexts) {
-  let texts; // textsを先に宣言
-  if (
-    currentTexts.charAt(0) === "[" &&
-    currentTexts.charAt(currentTexts.length - 1) === "]"
-  ) {
-    texts = currentTexts.slice(1, -1);
-  } else {
-    texts = currentTexts;
-  }
-
-  let count = 0;
-  let result = "";
-  for (const text of texts) {
-    if (text === "{") {
-      count++;
-    } else if (text === "}") {
-      count--;
-    }
-    if (count === 0 && text === ",") {
-      continue;
-    }
-    result += text;
-  }
-  return result;
-}
-
 const accessToken = `${process.env.SLIDE_JSON_ACCESS_TOKEN}`;
 
-// Octokitのインスタンスを作成
-const octokit = new Octokit({
-  auth: accessToken, // 認証トークンを設定
-});
+const owner = "enbipaipu";
+const repo = "test";
+const github_filePath = `https://api.github.com/repos/enbipaipu/test/contents/slid.json`;
 
-// デプロイ対象のリポジトリ情報
-const owner = "enbipaipu"; // リポジトリの所有者
-const repo = "test"; // リポジトリ名
-
-// ファイルの内容を変更
-const github_filePath = `https://api.github.com/repos/enbipaipu/test/contents/slid.json`; // 変更するファイルのパス
-
-// ファイルの変更をコミット
 async function deploy_json() {
   try {
     const filePath = path.join(__dirname, "test.json");
     const testJsonData = await fs.readFile(filePath, "utf8");
 
-    const changed = change(testJsonData);
+    const jsonData = JSON.parse(testJsonData);
+    const newDataToSendToGitHub = {
+      jsonData,
+    };
 
-    const jsonData = JSON.parse(changed);
-    const newFileContent = Buffer.from(jsonData).toString("base64");
+    const githubData = Buffer.from(
+      JSON.stringify(newDataToSendToGitHub)
+    ).toString("base64");
 
-    const {
-      data: { sha },
-    } = await octokit.repos.getContents({ owner, repo, path: github_filePath });
+    const octokit = new Octokit({
+      auth: accessToken,
+    });
 
-    // 変更内容をコミット
+    const getResponse = await fetch(github_filePath, {
+      headers: {
+        Authorization: accessToken,
+        Accept: "application/vnd.github.v3+json",
+      },
+    });
+
+    const getfiledata = getResponse.json();
+
     await octokit.repos.createOrUpdateFileContents({
       owner,
       repo,
       path: github_filePath,
-      message: "Update file content",
-      content: newFileContent,
-      sha: sha,
+      message: "Update data file",
+      content: githubData,
+      sha: getfiledata.sha,
     });
 
-    console.log("File updated successfully!");
+    console.log("Data sent to GitHub successfully!");
   } catch (error) {
-    console.error("Error updating file:", error);
+    console.error("Error:", error);
   }
 }
 
