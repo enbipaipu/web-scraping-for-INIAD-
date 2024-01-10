b9b26d704c846a214c2c68ce5af8c59e0649ce92
enbipaipu <s1f102300122@iniad.org>
2024-01-10 10:12:52 +0900

    コメントアウトを削除

--- lambda/get_json.js
+++ lambda/get_json.js
@@ -1,39 +1,6 @@
 const fetch = require("node-fetch");
 const fs = require("fs/promises");
 
-// function changes(currentTexts) {
-//   let texts; // textsを先に宣言
-//   // currentTextsの最初と最後の文字をチェックして条件に応じてtextsを定義
-//   if (
-//     currentTexts.charAt(0) === "[" &&
-//     currentTexts.charAt(currentTexts.length - 1) === "]"
-//   ) {
-//     texts = currentTexts.slice(1, -1);
-//   } else {
-//     texts = currentTexts;
-//   }
-
-//   let count = 0;
-//   let result = "[";
-//   for (const text of texts) {
-//     if (text === "{") {
-//       count++;
-//     } else if (text === "}") {
-//       count--;
-//     }
-//     result += text;
-//     if (count === 0 && text === "}") {
-//       result += ",";
-//     }
-//   }
-
-//   // 正確に最後のカンマを取り除く
-//   result = result.replace(/,\s*$/, "");
-
-//   result += "]";
-//   return result;
-// }
-
 const accessToken = `${process.env.SLIDE_JSON_ACCESS_TOKEN}`;
 
 const owner = "jun-eg";
