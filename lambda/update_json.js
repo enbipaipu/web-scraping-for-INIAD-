82c11bd9a041662833f9862735b4132cf71817fa
enbipaipu <s1f102300122@iniad.org>
2024-01-30 13:57:38 +0900

    <update> moocsからタイトルを取得

--- lambda/update_json.js
+++ lambda/update_json.js
@@ -1,10 +1,25 @@
-const fetch = require("node-fetch");
+const { default: axios } = require("axios");
+const { load } = require("cheerio");
+
+async function scrape(url) {
+  try {
+    const response = await axios.get(url);
+    const $ = load(response.data);
+
+    const title = $("title").text();
+    console.log(`ページのタイトル：${title}`);
+  } catch (error) {
+    console.log(`エラーが発生しました：${error}`);
+  }
+}
 
 async function update_json() {
   console.log("jsonのアップデートを行います。");
-
+  scrape("https://moocs.iniad.org/courses");
   console.log("jsonのアップデートが終了しました");
   return;
 }
+
+update_json();
 
 module.exports.update_json = update_json;
