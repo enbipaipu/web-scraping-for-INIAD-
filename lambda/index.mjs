3cd6c80c947b861ec12e51b6448db613f0610e24
enbipaipu <s1f102300122@iniad.org>
2024-02-01 18:53:35 +0900

    fix js -> mjs2

--- lambda/index.mjs
+++ lambda/index.mjs
@@ -7,7 +7,7 @@ async function main() {
   await get_json(); //jsonから保存済みのURLの情報を取得する関数。
   await update_json(); //infoにないURLをmoocsから取得する関数
   await deploy_json(); //new_infoをjsonにデプロイする関数。
-  console.log("mainを終了します");
+  console.log("mainを終了しま");
 }
 
 main();
